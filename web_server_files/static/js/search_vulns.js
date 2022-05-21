d058da2d46665881fae8a7adaafb3efc2756e507
Dustin Born <dustin.born@gmx.de>
2022-05-21 20:36:27 +0200

    Add CSV and Markdown export of vulnerability table

--- web_server_files/static/js/search_vulns.js
+++ web_server_files/static/js/search_vulns.js
@@ -4,70 +4,93 @@ var ignoreGeneralCpeVulns = false;
 var iconUnsorted = '<i class="fa-solid fa-sort"></i>';
 var iconSortDesc = '<i class="fa-solid fa-sort-down"></i>';
 var iconSortAsc = '<i class="fa-solid fa-sort-up"></i>';
+var curSortColIdx = 1, curSortColAsc = false;
+var showAsButtonsHTML = `<div class="row mt-3 justify-content-center align-items-center"><div class="col-6 text-center justify-content-center align-items-center"><button type="button" class="btn btn-info" name="copyCSVButton" id="copyCSVButton" onclick="copyToClipboardCSV()"><i style="font-size: 1rem" class="fa-solid fa-clipboard"></i>&nbsp;&nbsp;Copy to Clipboard (CSV)</button></div><div class="col-6 text-center justify-content-center align-items-center"><button type="button" class="btn btn-info" name="copyMarkdownTableButton" id="copyMarkdownTableButton" onclick="copyToClipboardMarkdownTable()"><i style="font-size: 1rem" class="fa-solid fa-clipboard"></i>&nbsp;&nbsp;Copy to Clipboard (Markdown Table)</button></div></div>`;
 
-
-function htmlEntities (text) {
-    return text.replace(/[\u00A0-\u9999<>\&"']/g, function(i) {
-        return '&#'+i.charCodeAt(0)+';';
+function htmlEntities(text) {
+    return text.replace(/[\u00A0-\u9999<>\&"']/g, function (i) {
+        return '&#' + i.charCodeAt(0) + ';';
     });
 }
 
-function createVulnsHtml(sortColumnIdx, asc) {
+function getCurrentVulnsSorted() {
+    var vulns = Object.values(Object.values(curVulnData)[0]);
+    if (curSortColIdx == 0) {  // CVE-ID
+        if (curSortColAsc) {
+            return vulns.sort(function (vuln1, vuln2) {
+                return vuln1.id.localeCompare(vuln2.id);
+            });
+        }
+        else {
+            return vulns.sort(function (vuln1, vuln2) {
+                return vuln1.id.localeCompare(vuln2.id);
+            }).reverse();
+
+        }
+    }
+    else if (curSortColIdx == 1) {  // CVSS
+        if (curSortColAsc) {
+            return vulns.sort(function (vuln1, vuln2) {
+                return parseFloat(vuln1.cvss) - parseFloat(vuln2.cvss);
+            });
+        }
+        else {
+            return vulns.sort(function (vuln1, vuln2) {
+                return parseFloat(vuln2.cvss) - parseFloat(vuln1.cvss);
+            });
+        }
+    }
+    else if (curSortColIdx == 3) {  // Exploits
+        if (curSortColAsc) {
+            return vulns.sort(function (vuln1, vuln2) {
+                exploits1 = vuln1.exploits || [];
+                exploits2 = vuln2.exploits || [];
+                return parseInt(exploits1.length) - parseInt(exploits2.length);
+            });
+        }
+        else {
+            return vulns.sort(function (vuln1, vuln2) {
+                exploits1 = vuln1.exploits || [];
+                exploits2 = vuln2.exploits || [];
+                return parseInt(exploits2.length) - parseInt(exploits1.length);
+            });
+        }
+    }
+}
+
+function createVulnsHtml() {
     var sortIconCVEID = iconUnsorted, sortFunctionCVEID = "reorderVulns(0, false)";
     var sortIconCVSS = iconUnsorted, sortFunctionCVSS = "reorderVulns(1, false)";
     var sortIconExploits = iconUnsorted, sortFunctionExploits = "reorderVulns(3, false)";
 
     // retrieve and sort vulns
-    vulns = Object.values(Object.values(curVulnData)[0]);
-    if (sortColumnIdx == 0) {  // CVE-ID
-        if (asc) {
-            vulns = vulns.sort(function (vuln1, vuln2) {
-                return vuln1.id.localeCompare(vuln2.id);
-            });
+    var vulns = getCurrentVulnsSorted();
+    if (curSortColIdx == 0) {  // CVE-ID
+        if (curSortColAsc) {
             sortIconCVEID = iconSortAsc;
             sortFunctionCVEID = "reorderVulns(0, false)";
         }
         else {
-            vulns = vulns.sort(function (vuln1, vuln2) {
-                return vuln1.id.localeCompare(vuln2.id);
-            });
-            vulns = vulns.reverse();
             sortIconCVEID = iconSortDesc;
             sortFunctionCVEID = "reorderVulns(0, true)";
         }
     }
-    else if (sortColumnIdx == 1) {  // CVSS
-        if (asc) {
-            vulns = vulns.sort(function (vuln1, vuln2) {
-                return parseFloat(vuln1.cvss) - parseFloat(vuln2.cvss);
-            });
+    else if (curSortColIdx == 1) {  // CVSS
+        if (curSortColAsc) {
             sortIconCVSS = iconSortAsc;
             sortFunctionCVSS = "reorderVulns(1, false)";
         }
         else {
-            vulns = vulns.sort(function (vuln1, vuln2) {
-                return parseFloat(vuln2.cvss) - parseFloat(vuln1.cvss);
-            });
             sortIconCVSS = iconSortDesc;
             sortFunctionCVSS = "reorderVulns(1, true)";
         }
     }
-    else if (sortColumnIdx == 3) {  // Exploits
-        if (asc) {
-            vulns = vulns.sort(function (vuln1, vuln2) {
-                exploits1 = vuln1.exploits || [];
-                exploits2 = vuln2.exploits || [];
-                return parseInt(exploits1.length) - parseInt(exploits2.length);
-            });
+    else if (curSortColIdx == 3) {  // Exploits
+        if (curSortColAsc) {
             sortIconExploits = iconSortAsc;
             sortFunctionExploits = "reorderVulns(3, false)";
         }
         else {
-            vulns = vulns.sort(function (vuln1, vuln2) {
-                exploits1 = vuln1.exploits || [];
-                exploits2 = vuln2.exploits || [];
-                return parseInt(exploits2.length) - parseInt(exploits1.length);
-            });
             sortIconExploits = iconSortDesc;
             sortFunctionExploits = "reorderVulns(3, true)";
         }
@@ -76,10 +99,10 @@ function createVulnsHtml(sortColumnIdx, asc) {
     vulns_html = '<table class="table table-sm table-rounded table-striped">';
     vulns_html += '<thead class="bg-darker">';
     vulns_html += '<tr>'
-    vulns_html += `<th onclick="${sortFunctionCVEID}">CVE-ID&nbsp;&nbsp;${sortIconCVEID}</th>`;
-    vulns_html += `<th onclick="${sortFunctionCVSS}">CVSS&nbsp;&nbsp;${sortIconCVSS}</th>`;
+    vulns_html += `<th onclick="${sortFunctionCVEID}" style="white-space: nowrap;">CVE-ID&nbsp;&nbsp;${sortIconCVEID}</th>`;
+    vulns_html += `<th onclick="${sortFunctionCVSS}" style="white-space: nowrap;">CVSS&nbsp;&nbsp;${sortIconCVSS}</th>`;
     vulns_html += '<th>Description</th>'
-    vulns_html += `<th onclick="${sortFunctionExploits}">Exploit&nbsp;&nbsp;${sortIconExploits}</th>`;
+    vulns_html += `<th onclick="${sortFunctionExploits}" style="white-space: nowrap;">Exploit&nbsp;&nbsp;${sortIconExploits}</th>`;
     vulns_html += "</tr></thead>";
     vulns_html += "<tbody>";
     vulns_html += "<tr></<tr>";  // make striping start with white
@@ -114,6 +137,75 @@ function createVulnsHtml(sortColumnIdx, asc) {
     return vulns_html;
 }
 
+function createVulnsMarkDownTable() {
+    var vulns = getCurrentVulnsSorted();
+    var vulns_md = "";
+    var has_exploits = false;
+
+    for (var i = 0; i < vulns.length; i++) {
+        if (vulns[i].exploits !== undefined && vulns[i].exploits.length > 0) {
+            has_exploits = true;
+            break
+        }
+    }
+
+    if (has_exploits) {
+        vulns_md = '|CVE|CVSS|Description|Exploits|\n';
+        vulns_md += '|:---:|:---:|:---|:---|\n';
+    }
+    else {
+        vulns_md = '|CVE|CVSS|Description|\n';
+        vulns_md += '|:---:|:---:|:---|\n';
+    }
+
+    for (var i = 0; i < vulns.length; i++) {
+        vulns_md += `|${vulns[i]["id"].replaceAll('-', '&#8209;')}|${vulns[i]["cvss"]}&nbsp;(v${vulns[i]["cvss_ver"]})|`;
+        vulns_md += `${htmlEntities(vulns[i]["description"]).replaceAll('|', '&#124;')}|`;
+
+        if (vulns[i].exploits !== undefined && vulns[i].exploits.length > 0)
+            vulns_md += `${vulns[i].exploits.join("<br>")}|`;
+        else if (has_exploits)
+            vulns_md += "|";
+
+        vulns_md += '\n'
+    }
+
+    return vulns_md;
+}
+
+function createVulnsCSV() {
+    var vulns = getCurrentVulnsSorted();
+    var vulns_csv = "";
+    var has_exploits = false;
+
+    for (var i = 0; i < vulns.length; i++) {
+        if (vulns[i].exploits !== undefined && vulns[i].exploits.length > 0) {
+            has_exploits = true;
+            break
+        }
+    }
+
+    if (has_exploits)
+        vulns_csv = 'CVE,CVSS,Description,Exploits\n';
+    else
+        vulns_csv = 'CVE,CVSS,Description\n';
+
+    for (var i = 0; i < vulns.length; i++) {
+        vulns_csv += `${vulns[i]["id"]},${vulns[i]["cvss"]} (v${vulns[i]["cvss_ver"]}),`;
+        vulns_csv += `"${vulns[i]["description"].replaceAll('"', '""')}"`;
+
+        if (has_exploits)
+            vulns_csv += ",";
+
+        if (vulns[i].exploits !== undefined && vulns[i].exploits.length > 0)
+            vulns_csv += `"${vulns[i].exploits.join(", ")}"`;
+        vulns_csv += '\n'
+    }
+
+    return vulns_csv;
+}
+
+
 function searchVulns() {
     var query = $('#query').val();
     var queryEnc = encodeURIComponent(query);
@@ -128,6 +220,8 @@ function searchVulns() {
     history.pushState({}, null, new_url);  // update URL
     $("#searchVulnsButton").attr("disabled", true);
     $("#vulns").html('<div class="row mt-3 justify-content-center align-items-center"><h5 class="spinner-border text-primary" style="width: 3rem; height: 3rem"></h5></div>');
+    curSortColIdx = 1;
+    curSortColAsc = false;
     curVulnData = {};
 
     $.get({
@@ -141,6 +235,8 @@ function searchVulns() {
                 else {
                     curVulnData = vulns;
                     vulns_html = createVulnsHtml(1, false);
+                    vulns_html += `<hr style="height: 2px; border:none; border-radius: 10px 10px 10px 10px; background-color:#d7d4d4;"/>`;
+                    vulns_html += showAsButtonsHTML;
                 }
             } else {
                 vulns_html = `<h5 class="text-center">No known vulnerabilities could be found for '${htmlEntities(query)}'</h5>`;
@@ -162,7 +258,11 @@ function searchVulns() {
 }
 
 function reorderVulns(sortColumnIdx, asc) {
+    curSortColIdx = sortColumnIdx;
+    curSortColAsc = asc;
     vulns_html = createVulnsHtml(sortColumnIdx, asc);
+    vulns_html += `<hr style="height: 2px; border:none; border-radius: 10px 10px 10px 10px; background-color:#d7d4d4;"/>`;
+    vulns_html += showAsButtonsHTML;
     $("#vulns").html(vulns_html);
 }
 
@@ -170,6 +270,18 @@ function ignoreGeneralVulnsToggle() {
     ignoreGeneralCpeVulns = !ignoreGeneralCpeVulns;
     $("#vulns").html('');
     curVulnData = {};
+}
+
+function copyToClipboardMarkdownTable() {
+    navigator.clipboard.writeText(createVulnsMarkDownTable());
+    $("#copyMarkdownTableButton").html('<i style="font-size: 1rem" class="fa-solid fa-clipboard-check"></i>&nbsp;&nbsp;Copied Markdown Table to Clipboard');
+    $("#copyMarkdownTableButton").attr('class', 'btn btn-success');
+}
+
+function copyToClipboardCSV() {
+    navigator.clipboard.writeText(createVulnsCSV());
+    $("#copyCSVButton").html('<i style="font-size: 1rem" class="fa-solid fa-clipboard-check"></i>&nbsp;&nbsp;Copied CSV to Clipboard');
+    $("#copyCSVButton").attr('class', 'btn btn-success');
 }
 
 function init() {
@@ -200,6 +312,6 @@ $("#query").keypress(function (event) {
 init();
 
 // activate tooltips
-$(document).ready(function() {
+$(document).ready(function () {
     $("body").tooltip({ selector: '[data-toggle=tooltip]' });
 });
