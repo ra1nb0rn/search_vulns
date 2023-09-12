936c8710568a66b36838fb69333d33edd67e5dbc
Dustin Born <dustin.born@gmx.de>
2023-09-12 22:26:01 +0200

    Return combined exit code and make scripts executable

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -5,9 +5,16 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo '[+] Running test_cve_completeness.py'
 python3 "${SCRIPT_DIR}/test_cve_completeness.py"
+EXIT_1=$?
 echo '[+] Running test_cve_attr_completeness.py'
 python3 "${SCRIPT_DIR}/test_cve_attr_completeness.py"
+EXIT_2=$?
 echo '[+] Running test_exploit_completeness.py'
 python3 "${SCRIPT_DIR}/test_exploit_completeness.py"
+EXIT_3=$?
 echo '[+] Running test_related_queries.py'
 python3 "${SCRIPT_DIR}/test_related_queries.py"
+EXIT_4=$?
+
+# https://stackoverflow.com/a/16358989
+! (( $EXIT_1 || $EXIT_2 || $EXIT_3 || $EXIT_4 ))
