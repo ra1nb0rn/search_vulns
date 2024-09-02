4c0f55618ccde322f8da7f70d3f813495b9cd1ad
Dustin Born <dustin.born@gmx.de>
2024-09-02 22:47:13 +0200

    Wrap around when navigating CPE suggestions via arrow keys

--- web_server_files/static/js/search_vulns.js
+++ web_server_files/static/js/search_vulns.js
@@ -1114,9 +1114,11 @@ function moveCPESuggestionUpDown(event) {
     else if (event.keyCode == 40)
         curSelectedCPESuggestion++;
 
-    // enforce lower and upper bounds
-    curSelectedCPESuggestion = Math.max(-1, curSelectedCPESuggestion);
-    curSelectedCPESuggestion = Math.min(curSelectedCPESuggestion, $("#cpeSuggestions").find('ul').children('li').length - 1);
+    // wrap around if moving below available suggestions or above them
+    if (curSelectedCPESuggestion < 0)
+        curSelectedCPESuggestion = $("#cpeSuggestions").find('ul').children('li').length - 1;
+    else if (curSelectedCPESuggestion > $("#cpeSuggestions").find('ul').children('li').length - 1)
+        curSelectedCPESuggestion = 0;
 
     if (curSelectedCPESuggestion > -1) {
         const suggestionElement = $('#cpe-suggestion-' + curSelectedCPESuggestion);
