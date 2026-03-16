4050a34473a57e5a4184d5e0ce755f987726248e
Dustin Born <dustin.born@gmx.de>
2026-03-16 19:12:29 +0100

    Add Vulncheck NVD++ rate limiting
    Rate limiting was introduced recently:
    https://docs.vulncheck.com/changelog/2026-02-25

--- src/search_vulns/modules/vulncheck/build.py
+++ src/search_vulns/modules/vulncheck/build.py
@@ -13,7 +13,7 @@ LOGGER = logging.getLogger()
 VULNCHECK_UPDATE_SUCCESS = None
 
 
-async def vulncheck_worker(cveids, headers):
+async def vulncheck_worker(cveids, headers, rate_limit):
     """Pull cve<->cpe data for the given cveids asyncronously and return results"""
 
     global VULNCHECK_UPDATE_SUCCESS
@@ -21,72 +21,73 @@ async def vulncheck_worker(cveids, headers):
     affects_statements = []
     retry_limit = 12
     initial_retry_interval = 3
-    async with aiohttp.ClientSession() as session:
-        for cveid in cveids:
-            if VULNCHECK_UPDATE_SUCCESS is not None and not VULNCHECK_UPDATE_SUCCESS:
-                return []
+    async with rate_limit:
+        async with aiohttp.ClientSession() as session:
+            for cveid in cveids:
+                if VULNCHECK_UPDATE_SUCCESS is not None and not VULNCHECK_UPDATE_SUCCESS:
+                    return []
 
-            # get extra data from vulncheck API
-            vulncheck_data = None
-            for i in range(retry_limit + 1):
-                try:
-                    vulncheck_url = "https://api.vulncheck.com/v3/index/nist-nvd2"
-                    params = {"cve": cveid}
-                    vulncheck_response = await session.get(
-                        url=vulncheck_url, headers=headers, params=params
-                    )
-                    if vulncheck_response.status == 200 and vulncheck_response.text is not None:
-                        vulncheck_data = await vulncheck_response.json()
-                        if vulncheck_data is not None:
-                            break
-                except Exception as e:
-                    if VULNCHECK_UPDATE_SUCCESS is None:
-                        LOGGER.warning(
-                            "Got an exception when downloading data from vulncheck API: %s"
-                            % str(e)
+                # get extra data from vulncheck API
+                vulncheck_data = None
+                for i in range(retry_limit + 1):
+                    try:
+                        vulncheck_url = "https://api.vulncheck.com/v3/index/nist-nvd2"
+                        params = {"cve": cveid}
+                        vulncheck_response = await session.get(
+                            url=vulncheck_url, headers=headers, params=params
                         )
-                await asyncio.sleep(initial_retry_interval + i * 3)
-
-            if vulncheck_data is None:
-                if VULNCHECK_UPDATE_SUCCESS is None:
-                    LOGGER.warning("Could not get vulncheck data for: %s" % str(cveid))
-                VULNCHECK_UPDATE_SUCCESS = False
-                return []
-
-            # extract CPE configurations data from vulncheck's extra data
-            if vulncheck_data.get("data", []):
-                for vc_configs_entry in vulncheck_data["data"][0].get("vcConfigurations", []):
-                    for vc_configs_node in vc_configs_entry.get("nodes", []):
-                        for cpe_entry in vc_configs_node.get("cpeMatch", []):
-                            if not cpe_entry["vulnerable"]:
-                                continue
-                            cpe = cpe_entry["criteria"]
-                            cpe_version_start, cpe_version_end = "", ""
-                            is_cpe_version_start_incl, is_cpe_version_end_incl = False, False
-                            if "versionStartIncluding" in cpe_entry:
-                                cpe_version_start = cpe_entry["versionStartIncluding"]
-                                is_cpe_version_start_incl = True
-                            elif "versionStartExcluding" in cpe_entry:
-                                cpe_version_start = cpe_entry["versionStartExcluding"]
-                                is_cpe_version_start_incl = False
-
-                            if "versionEndIncluding" in cpe_entry:
-                                cpe_version_end = cpe_entry["versionEndIncluding"]
-                                is_cpe_version_end_incl = True
-                            elif "versionEndExcluding" in cpe_entry:
-                                cpe_version_end = cpe_entry["versionEndExcluding"]
-                                is_cpe_version_end_incl = False
-
-                            affects_statements.append(
-                                (
-                                    cveid,
-                                    cpe,
-                                    cpe_version_start,
-                                    is_cpe_version_start_incl,
-                                    cpe_version_end,
-                                    is_cpe_version_end_incl,
-                                )
+                        if vulncheck_response.status == 200 and vulncheck_response.text is not None:
+                            vulncheck_data = await vulncheck_response.json()
+                            if vulncheck_data is not None:
+                                break
+                    except Exception as e:
+                        if VULNCHECK_UPDATE_SUCCESS is None:
+                            LOGGER.warning(
+                                "Got an exception when downloading data from vulncheck API: %s"
+                                % str(e)
                             )
+                    await asyncio.sleep(initial_retry_interval + i * 3)
+
+                if vulncheck_data is None:
+                    if VULNCHECK_UPDATE_SUCCESS is None:
+                        LOGGER.warning("Could not get vulncheck data for: %s" % str(cveid))
+                    VULNCHECK_UPDATE_SUCCESS = False
+                    return []
+
+                # extract CPE configurations data from vulncheck's extra data
+                if vulncheck_data.get("data", []):
+                    for vc_configs_entry in vulncheck_data["data"][0].get("vcConfigurations", []):
+                        for vc_configs_node in vc_configs_entry.get("nodes", []):
+                            for cpe_entry in vc_configs_node.get("cpeMatch", []):
+                                if not cpe_entry["vulnerable"]:
+                                    continue
+                                cpe = cpe_entry["criteria"]
+                                cpe_version_start, cpe_version_end = "", ""
+                                is_cpe_version_start_incl, is_cpe_version_end_incl = False, False
+                                if "versionStartIncluding" in cpe_entry:
+                                    cpe_version_start = cpe_entry["versionStartIncluding"]
+                                    is_cpe_version_start_incl = True
+                                elif "versionStartExcluding" in cpe_entry:
+                                    cpe_version_start = cpe_entry["versionStartExcluding"]
+                                    is_cpe_version_start_incl = False
+
+                                if "versionEndIncluding" in cpe_entry:
+                                    cpe_version_end = cpe_entry["versionEndIncluding"]
+                                    is_cpe_version_end_incl = True
+                                elif "versionEndExcluding" in cpe_entry:
+                                    cpe_version_end = cpe_entry["versionEndExcluding"]
+                                    is_cpe_version_end_incl = False
+
+                                affects_statements.append(
+                                    (
+                                        cveid,
+                                        cpe,
+                                        cpe_version_start,
+                                        is_cpe_version_start_incl,
+                                        cpe_version_end,
+                                        is_cpe_version_end_incl,
+                                    )
+                                )
 
     if VULNCHECK_UPDATE_SUCCESS is not None and not VULNCHECK_UPDATE_SUCCESS:
         return []
@@ -115,7 +116,7 @@ async def full_update_async(productdb_config, vulndb_config, module_config, stop
 
         # retrieve CVEs with only one general CPE affected
         db_cursor.execute(
-            'SELECT DISTINCT cve_id FROM nvd_cpe WHERE cpe LIKE "cpe:2.3:%:%:%:*:*:*:*:*:*:*:*" AND cpe_version_start = "" AND cpe_version_end = "" AND cve_id IN (SELECT cve_id FROM nvd_cpe GROUP BY cve_id HAVING COUNT(*) = 1);'
+            'SELECT DISTINCT cve_id FROM nvd_cpe WHERE cpe LIKE "%:*:*:*:*:*:*:*:*" AND cpe_version_start = "" AND cpe_version_end = "" AND cve_id IN (SELECT cve_id FROM nvd_cpe GROUP BY cve_id HAVING COUNT(*) = 1);'
         )
         cves_one_general_cpe = [result[0] for result in db_cursor.fetchall()]
 
@@ -124,10 +125,13 @@ async def full_update_async(productdb_config, vulndb_config, module_config, stop
         headers = {"Authorization": "Bearer %s" % vulncheck_api_key}
         tasks = []
 
-        # vulncheck API does not require a rate limit, see https://vulncheck.com/blog/nvd-plus-plus
+        # ~~vulncheck API does not require a rate limit, see https://vulncheck.com/blog/nvd-plus-plus~~
+        # update: vulncheck API now has a rate limit of 1000 requests per minute: https://docs.vulncheck.com/api#rate-limiting
+
+        rate_limit = AsyncLimiter(999, 60)
         while offset < len(vulncheck_cves):
             cveids = vulncheck_cves[offset : min(offset + batchsize, len(vulncheck_cves))]
-            task = asyncio.create_task(vulncheck_worker(cveids=cveids, headers=headers))
+            task = asyncio.create_task(vulncheck_worker(cveids=cveids, headers=headers, rate_limit=rate_limit))
             tasks.append(task)
             offset += batchsize
 
